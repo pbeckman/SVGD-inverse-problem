e294af553b859b064bf971dc6f3c7a31133abd3d
pbeckman <pbeckman@uchicago.edu>
2022-12-01 11:43:14 -0500

    PDE example is still fragile, but the plots are better

--- pde-example.jl
+++ pde-example.jl
@@ -6,17 +6,20 @@ include("./svgd.jl")
 function plot_distribution(x, logc, c_true; title="", legend=:none)
   M = length(logc)
   pl = plot(
-    x, exp.(sum(logc) / M), 
-    linewidth=2, label="SVGD posterior mean", 
+    x, sum(logc) / M, 
+    linewidth=2, marker=2, label="SVGD posterior mean (±2σ)", 
+    ribbon=2sqrt.(sum((.^).(logc, 2)) / M), fillalpha=0.5,
     title=title, legend=legend
     )
   # xgrid = range(0, stop=1, length=100)
   plot!(pl, 
-    x, c_true, 
-    linewidth=2, label="True permeability"
+    x, log.(c_true), 
+    linewidth=2, marker=2, label="True log-permeability"
     )
   return pl
 end
+
+println("Setting up problem...\n")
 
 # squared exponential kernel function for SVGD space
 k(xi, xj; l=1.0)      = exp(-norm(xi - xj)^2 / l^2)
@@ -37,25 +40,32 @@ function solve_poisson(c, f, a, b)
   A[1, 1:2]         .= [1, 0]
   A[end, end-1:end] .= [0, 1]
 
+  # if eltype(c) == Float64
+  #   @show c
+  # else
+  #   @show getfield.(c, :value)
+  # end
+
   # solve PDE
   u = (A \ vcat(a, f, b))[2:end-1]
-
+  
   return u
 end
 
-# regular grid
-n = 10
+# regular grid for observations and PDE discretization
+n = 20
 x = range(0, stop=1.0, length=n)
 # boundary conditions u(0) = a, u(1) = b
 a, b = 1, -1
 # forcing
 f = zeros(n-2)
 # true permeability
-c_true = 0.5 .+ x .^ 2
+c_true = 0.1 .+ 0.5*x .^ 2
 # iid noise variance for gaussian likelihood
 s2 = 1e-2
 # generate data
-y = solve_poisson(c_true, f, a, b) .+ sqrt(s2)*randn(n-2)
+u_true = solve_poisson(c_true, f, a, b)
+y      = u_true .+ sqrt(s2) * randn(n-2)
 
 # Matern nu = 3/2 covariance function for prior
 function k_prior(xi, xj; sc=1.0, l=1.0) 
@@ -72,29 +82,51 @@ function logp(logc, f, a, b, y, s2, K_prior)
 end
 grad_logp(logc) = gradient(logc -> logp(logc, f, a, b, y, s2, K_prior), logc)
 
+# lengthscale for SVGD kernel
+l = 0.01
 # choose initial particles from prior
-M    = 2
+M    = 20
 logc = collect.(eachcol(cholesky(K_prior).L * randn(n, M)))
 
 # iteration parameters
-step_size = 1e-3
-iter      = 1000
+step_size = 1e-5
+iter      = 10000
 
 # run SVGD and make some intermediate plots
 gr(size=(300, 300))
 pls = []
 push!(pls, plot_distribution(x, logc, c_true, title="Iteration 0", legend=:topright))
 display(pls[end])
-for i=1:4
-  println("Running SVGD iterations $((i-1)*Int64(iter/4)) - $(i*Int64(iter/4))...")
-  logc .= SVGD(k, grad_k, grad_logp, logc, step_size, iter/4, verbose=true)
-  push!(pls, plot_distribution(x, logc, c_true, title="Iteration $(i*Int64(iter/4))"))
+for i=1:5
+  println("Running SVGD iterations $((i-1)*Int64(iter/5)) - $(i*Int64(iter/5))...")
+  logc .= SVGD(
+    (xi, xj) -> k(xi, xj, l=l), (xi, xj) -> grad_k(xi, xj, l=l), 
+    grad_logp, logc, 
+    step_size, iter/5, 
+    verbose=false
+    )
+  push!(pls, plot_distribution(x, logc, c_true, title="Iteration $(i*Int64(iter/5))"))
   display(pls[end])
 end
 
 # plot all histograms
-l = @layout [a b c d e]
-pl = plot(pls..., layout=l, size=(1500, 300), margin=5mm)
+pl_data = plot(
+  x, vcat(a, u_true, b), 
+  label="True solution",
+  linewidth=3, marker=2,  c=:blue
+  )
+plot!(pl_data,
+  x, vcat(a, solve_poisson(exp.(sum(logc) / M), f, a, b), b), 
+  label="Solution with mean permeability",
+  linewidth=3, marker=2, c=:red, linestyle=:dash
+  )
+scatter!(pl_data, 
+  x[2:end-1], y, 
+  label="Noisy data",
+  marker=5, c=:black
+  )
+l = @layout [a{0.4w} grid(2,3)]
+pl = plot(pl_data, pls..., layout=l, size=(1500, 500), margin=5mm)
 display(pl)
 println("\nDisplaying final plot...")
 readline(stdin)
