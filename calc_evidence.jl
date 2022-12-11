c9e38c4d106a6cb1281a9bbf8b632f9e61f60b64
Nolan Reilly <nolan.reilly@nyu.edu>
2022-12-11 15:54:09 -0500

    Evidence calculation is now plausible

--- calc_evidence.jl
+++ calc_evidence.jl
@@ -30,43 +30,47 @@ function plot_distribution(x, logc, c_true; title="", legend=:none, ylabel="")
   return pl
 end
 
-function L_mat(c)
-  n = length(c)
+function L_mat(m)
+  n = length(m)
+  c = exp.(m)
   h = 1/(n-1)
   L = 1/h^2 * Tridiagonal(
       c[1:end-1],
       -vcat(2c[1], c[2:end-1]+c[3:end], 2c[end]), 
       c[2:end]
   )
-  return L
+  L[1, 1:2]         .= [1, 0]
+  L[end, end-1:end] .= [0, 1]
+  return -L
 end
 
-function M_mat(u)
+function M_mat(u,m)
   # Implicitly applies c_{i+1} = 0 BCs on c!
   n = length(u)
   h = 1/(n-1)
-  M = 1/h^2 * Bidiagonal(
-      vcat(u[1], u[2:end].-u[1:end-1]),
-      u[2:end].-u[1:end-1],
+  z = [exp(m[i])/m[i]*(u[i]-u[i-1])/h for i = 2:n]
+  M = 1/h * Bidiagonal(
+      vcat(0.0,z[1:n-2],0.0),
+      vcat(0.0,-z[2:n-1]),
       :U
   )
   return M
 end
 
-function evidence(y, f, u_0, c_0, σ2, Λ, S)
-  # p(y|f, σ2, Λ, u_0, c_0)
+function evidence(y, f, u_0, m_0, σ2, Λ, S)
+  # p(y|f, σ2, Λ, u_0, m_0)
   # where f is PDE rhs [-∇.(c∇u) = f]
   # σ2 is Gaussian noise variance
-  # Λ is precision of Gaussian prior for c
-  # u_0, c_0 are (u,c) around which PDE is linearized
+  # Λ is precision of Gaussian prior for m
+  # u_0, m_0 are (u,m) around which PDE is linearized
   n = length(y)
-  L = L_mat(c_0)
+  L = L_mat(m_0)
   Linv = inv(L)
-  M = M_mat(u_0)
-  μ = L\(M*2f)
-  Σ = σ2*S*S' + S*Linv*M*Λ*M'*Linv'*S'
-  Sc = cholesky(Σ)
-  return 1/sqrt(2pi)*exp((y.-μ)'*(Sc\(y.-μ)) - 0.5*logdet(S))
+  M = M_mat(u_0, m_0)
+  μ = S*(L\(M*2f))
+  Σ = 1e-15I(n) + σ2*S*S' + S*Linv*M*Λ*M'*Linv'*S'
+  Sc = cholesky(0.5*(Σ+Σ'))
+  return -(y.-μ)'*(Sc\(y.-μ)) - 0.5*logdet(Sc) - 0.5*log(2pi)
 end
 
 function bisect_srch(x,Y)
@@ -96,7 +100,7 @@ function S_mat(xx,X)
     S[i,j] = d
     S[i,j+1] = 1-d
   end
-  return sparse(S)
+  return S
 end
 
 println("Setting up problem...\n")
@@ -108,7 +112,7 @@ c_true_func(x) = 1e-1*(1 + x^2 + 0.2sin(4pi*x))
 # iid noise variance for gaussian likelihood
 s2 = 1e-2
 # compute reference solution
-n_ref = 40
+n_ref = 15
 f_ref = ones(n_ref-2)
 x_ref = range(0, stop=1.0, length=n_ref)
 u_ref = solve_poisson(c_true_func.(x_ref), f_ref, a, b)
@@ -118,10 +122,6 @@ x = rand(n)
 # generate data
 c_true = c_true_func.(x_ref)
 y = sample(x, x_ref, u_ref) .+ sqrt(s2) * randn(n)
-# discretization of PDE
-nd = 15
-fd = ones(nd-2)
-xd = range(0, stop=1.0, length=nd)
 
 # Matern nu = 3/2 covariance function for prior
 sc_prior, l_prior = 0.01, 0.5
@@ -129,84 +129,56 @@ function k_prior(xi, xj)
   d = norm(xi - xj) / l_prior
   return sc_prior * (1 + sqrt(3)*d) * exp(-sqrt(3)*d)
 end
-# compute prior covariance matrix
-K_prior = [k_prior(xi, xj) for xi in xd, xj in xd]
-# select prior mean
-mu_prior = fill(sum(log.(c_true_func.(xd)) / nd), nd)
 
 # choose initial particles from prior
-M    = 10
-logc = collect.(eachcol(cholesky(K_prior).L * randn(nd, M) .+ mu_prior))
+np = 10
 
 # squared exponential kernel function for SVGD space
-l = sum([norm(xi - xj) for xi in logc, xj in logc]) / M^2
-l /= 1
-@show l
-k(xi, xj)      = exp(-norm(xi - xj)^2 / l^2)
-grad_k(xi, xj) = -2/l^2 * (xi - xj) * exp(-norm(xi - xj)^2/l^2)
+kl(xi, xj,l)      = exp(-norm(xi - xj)^2 / l^2)
+grad_kl(xi, xj,l) = -2/l^2 * (xi - xj) * exp(-norm(xi - xj)^2/l^2)
 
 # iteration parameters
 step_size = 1e-7
-iter      = 1000
+iter      = 10000
 
-# run SVGD and make some intermediate plots
-gr(size=(300, 300))
-pls = []
-push!(pls, plot_distribution(xd, logc, c_true_func.(xd), title="Iteration 0", legend=:topright, ylabel="logc"))
-display(pls[end])
-saveiters = [100, 1000, 10000]
-for i=1:length(saveiters)
-  println("Running SVGD iterations $(i == 1 ? 0 : saveiters[i-1]) - $(saveiters[i])...")
-  logc .= SVGD(
-    k, grad_k,
-    logc -> grad_logp(logc, xd, fd, a, b, x, y, s2, K_prior, mu_prior), logc, 
-    step_size, saveiters[i] - (i == 1 ? 0 : saveiters[i-1]), 
-    verbose=false
-    # bounds=(fill(-3, n), fill(-1, n))
-    )
-  push!(pls, plot_distribution(xd, logc, c_true_func.(xd), title="Iteration $(saveiters[i])"))
-  display(pls[end])
-end
-
-# plot data
-pl_data = plot(
-  x_ref, u_ref, 
-  label="true",
-  linewidth=3, c=:blue,
-  size=(300, 200)
-  )
-plot!(pl_data,
-  xd, solve_poisson(exp.(sum(logc) / M), fd, a, b), 
-  label="using post. mean",
-  linewidth=3, c=:red, linestyle=:dash
-  )
-scatter!(pl_data, 
-  x, y, 
-  label="data",
-  marker=4, c=:black
-  )
-display(pl_data)
-# readline(stdin)
-l = @layout [grid(2,2)]
-pl = plot(pls..., layout=l, size=(600, 600))
-gui(pl)
-println("\nDisplaying final plot...")
-
+println("Performing model selection\n")
 
 # Model selection
-n_trial = 8:17
+n_trial = 8:25
 ev_trial = zeros(size(n_trial))
-c_0 = reduce(.+,logc)
-u_0 = 
 for i = 1:length(n_trial)
+  # Setting up grid
   n_t = n_trial[i]
-  f_t = ones(n_trial)   # hax -- should correspond to f.(x_t)
+  f_t = ones(n_t-2)   # hax -- should correspond to f.(x_t)
   x_t = range(0, stop=1.0, length=n_t)
+
+  println("n = ",n_t)
+
+  # Calculating prior distribution
   K_prior = [k_prior(xi, xj) for xi in x_t, xj in x_t]
+  mu_prior = fill(sum(log.(c_true_func.(x_t)) / n_t), n_t)
   Kc = cholesky(K_prior)
+  logc = collect.(eachcol(cholesky(K_prior).L * randn(n_t, np) .+ mu_prior))
+  
+  # Repulsion functions for SVGD
+  l = sum([norm(xi - xj) for xi in logc, xj in logc]) / np^2
+  k(xi,xj) = kl(xi,xj,l)
+  grad_k(xi,xj) = grad_kl(xi,xj,l)
+  
+  # Find MAP logc for this grid
+  logc .= SVGD(
+    k, grad_k,
+    logc -> grad_logp(logc, x_t, f_t, a, b, x, y, s2, K_prior, mu_prior), logc, 
+    step_size, iter, 
+    verbose=false
+    )
+  c_0 = reduce(.+,logc)/length(logc)
+  u_0 = solve_poisson(c_0, f_t, a, b)
+  
+  # Linearize likelihood around this MAP solution and calculate evidence
   Λ = inv(Kc)
   S = S_mat(x,x_t)
-  ev_trial[i] = evidence(y, f_t, u_0, c_0, s2, Λ, S)
+  ev_trial[i] = evidence(y, vcat(a,f_t,b), u_0, c_0, s2, Λ, S)
 end
 
 gui(plot(n_trial, ev_trial, title="Evidence vs. number of grid pts"))
